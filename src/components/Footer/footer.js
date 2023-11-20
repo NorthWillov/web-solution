59eed9906b5c789640c15d800181b33859831b87
NorthWillov <northwillov@gmail.com>
2023-11-20 14:58:52 +0100

    Add contact section

--- src/components/Footer/footer.js
+++ src/components/Footer/footer.js
@@ -2,7 +2,26 @@ import React from "react";
 import styles from "./footer.module.css";
 
 function Footer() {
-  return <footer className={styles.main}></footer>;
+  return (
+    <footer className={styles.main}>
+      <div className={styles.contacts}>
+        <div className={styles.contactsQuote}>
+          <p>Unlock the Potential of Communication with LOREM IPSON</p>
+        </div>
+        <div className={styles.contact}>
+          <h4 className={styles.contactInfoTitle}>contact info</h4>
+          <div className={styles.contactInfo}>
+            <i class="fa-solid fa-envelope"></i>
+            <span>business@clubs.fun</span>
+          </div>
+          <div className={styles.contactInfo}>
+            <i class="fa-solid fa-envelope"></i>
+            <span>support@clubs.fun</span>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
