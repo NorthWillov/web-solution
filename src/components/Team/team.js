828595c682b881e48a4dbdcfb1c4d42a94a629ee
NorthWillov <northwillov@gmail.com>
2023-12-02 17:20:27 +0100

    Add slider

--- src/components/Team/team.js
+++ src/components/Team/team.js
@@ -1,14 +1,62 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Image from "next/image";
 import artem from "../../assets/artem.png";
 import den from "../../assets/den.png";
+import Carousel from "react-multi-carousel";
 import styles from "./team.module.css";
 
+const responsive = {
+  superLargeDesktop: {
+    breakpoint: { max: 4000, min: 3000 },
+    items: 5,
+  },
+  desktop: {
+    breakpoint: { max: 3000, min: 1024 },
+    items: 3,
+  },
+  tablet: {
+    breakpoint: { max: 1024, min: 464 },
+    items: 2,
+  },
+  mobile: {
+    breakpoint: { max: 464, min: 0 },
+    items: 2,
+  },
+};
+
 function Team(props) {
+  const [isDesktop, setIsDesktop] = useState();
+
+  useEffect(() => {
+    setIsDesktop(window.innerWidth >= 1050);
+    const handleResize = () => setIsDesktop(window.innerWidth >= 1050);
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
     <div className={styles.main}>
       <h1 className={styles.title}>Project Team</h1>
-      <div className={styles.cards}>
+      <Carousel
+        swipeable={!isDesktop}
+        draggable={!isDesktop}
+        showDots={!isDesktop}
+        responsive={responsive}
+        ssr={true}
+        infinite={true}
+        autoPlay={false}
+        autoPlaySpeed={1000}
+        keyBoardControl={true}
+        customTransition="all .5"
+        transitionDuration={500}
+        containerClass="carousel-container"
+        removeArrowOnDeviceType={["tablet", "mobile"]}
+        deviceType={"mobile"}
+        dotListClass="custom-dot-list-style"
+        itemClass="carousel-item-padding-40-px"
+      >
         <div className={styles.card}>
           <div className={styles.imgWrapper}>
             <Image
@@ -83,7 +131,7 @@ function Team(props) {
             </div>
           </div>
         </div>
-      </div>
+      </Carousel>
     </div>
   );
 }
